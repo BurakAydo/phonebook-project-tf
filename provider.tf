e1df1c4acf0dce589a4d2e026e40dea7ce84a5c6
BurakAydo <avburakaydogmus@gmail.com>
2023-03-28 11:15:36 +0300

    Phonebook-tf project created

---
+++ provider.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.58.0"
+    }
+    github = {
+      source = "integrations/github"
+      version = "5.18.3"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+  profile = "XXXXXXXXXXXXXXXXXXXXXX"
+//  access_key = ""
+//  secret_key = ""
+
+}
+
+provider "github" {
+  token = var.git-token
+  }
---
+++ var.tf
@@ -0,0 +1,16 @@
+variable "git-name" {
+  default = "BurakAydo"
+}
+
+variable "git-token" {
+  default = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+}
+
+variable "key-name" {
+  default = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+}
+
+variable "hosted-zone" {
+  default = "burakaydogmus.world"
+}
+
